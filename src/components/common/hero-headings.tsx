29b818d46996ba9a94ea35d66ed5316c71280001
diegovfeder <63731464+diegovfeder@users.noreply.github.com>
2024-10-08 10:36:31 -0300

    feat(content): updates content in contact-info and hero-headings

--- src/components/common/hero-headings.tsx
+++ src/components/common/hero-headings.tsx
@@ -5,8 +5,8 @@ import { sessionLink } from '@/lib/whatsapp'
 const HeroHeadings = () => {
   return (
     <>
-     <h2 className="pb-12 md:pb-16 mx-auto text-center text-3xl md:text-5xl font-extrabold">
-        O DESTINO PERFEITO PARA OS AMANTES DE WAKEBOARD
+     <h2 className="pb-12 md:pb-16 mx-auto text-center text-3xl md:text-5xl font-extrabold uppercase">
+        O destino perfeito para os amantes de Wakeboard!
       </h2>
       <p className="pb-8 text-start text-5xl md:text-7xl text-green-500 font-black tracking-tight">
         TERÇA A SEXTA:
@@ -19,12 +19,12 @@ const HeroHeadings = () => {
         <span className="font-semibold">09:00 - 19:00</span>
       </p>
       <Link
-        className="m-8 p-3 sm:p-4 flex text-center items-center justify-center self-center text-xl md:text-2xl font-black bg-green-600 hover:bg-green-700 shadow-sm hover:shadow-md shadow-green-600 hover:shadow-slate-800 rounded-md"
+        className="m-8 p-3 sm:p-4 flex text-center items-center justify-center self-center text-xl md:text-2xl font-black uppercase bg-green-600 hover:bg-green-700 shadow-sm hover:shadow-md shadow-green-600 hover:shadow-slate-800 rounded-md"
         href={sessionLink}
         target="_blank"
         rel="noopener noreferrer"
       >
-        FAÇA SEU AGENDAMENTO
+        Faça seu agendamento
       </Link>
     </>
   )
